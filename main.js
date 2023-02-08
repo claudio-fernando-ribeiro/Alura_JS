6689d1f3291dee4669c28e6d353179fd50d9d0ec
claudio-fernando-ribeiro <claudiofernando1989@gmail.com>
2023-02-08 11:16:31 -0300

    projFinal

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-function tocaSom(idElementAudio){
-    document.querySelector(idElementAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //Template string
-    const idAudio = `#som_${instrumento}`; 
-    tecla.onclick = function (){
-    tocaSom(idAudio);
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
-    contador ++;
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
